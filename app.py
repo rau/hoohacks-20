a3e56df18f1d3f1ed15f50114ba9a6e61a9814d6
Raunak <raunakdaga@gmail.com>
2020-03-29 19:33:55 -0400

    Added things

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask import send_file, make_response, send_from_directory
-from functions import json_to_list
+from functions import json_to_list, remove_from_list, add_to_list, text_info
+from keys import MAPBOX_TOKEN
 import os
 
 
 # Sets CWD to whatever directory app.py is located in
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 app = Flask(__name__, template_folder="templates", static_url_path='/static')
 
@@ -15,13 +17,32 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['DEBUG'] = True
 # app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    if 'visited' in request.cookies:
+        return render_template("index.html", list_new=json_to_list(), mbat=MAPBOX_TOKEN, show_faq=False)
+    response = make_response(render_template("index.html", list_new=json_to_list(), mbat=MAPBOX_TOKEN, show_faq=True))
+    response.set_cookie('visited', 'visited', max_age=60 * 60 * 24 * 365)
+    return response
 
-@app.route("/map")
+@app.route("/map/")
 def map(): 
-    return render_template("map.html", list_new=json_to_list())
+    return render_template("map.html", list_new=json_to_list(), mbat=MAPBOX_TOKEN)
+
+@app.route('/remove/<identifier>/', methods=['POST'])
+def remove(identifier: str):
+    if request.method == 'POST':
+        text_info(str(identifier), request.form['phone_number'])
+        remove_from_list(str(identifier))
+        return redirect('/')
+
+@app.route('/add/', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        add_to_list()
+        return redirect('/')
 
 
 app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,38 +1,42 @@
-from keys import API_KEY
+from keys import GOOGLE_API_KEY, ACCOUNT_SID, AUTH_TOKEN
+from twilio.rest import Client
 import sys
 import googlemaps
 import json
 import string
 import random
 
-def address_to_latlng():
-    str_value = sys.argv[1]
-    MAPS_API_KEY = API_KEY
+def address_to_lnglat(address):
+    str_value = address
+    MAPS_API_KEY = GOOGLE_API_KEY
     gmaps = googlemaps.Client(key=MAPS_API_KEY)
     geocode_result = gmaps.geocode(str_value)[0]
     lat, lng = geocode_result['geometry']['location']['lat'], geocode_result['geometry']['location']['lng']
-    return lat, lng
+    city = geocode_result['address_components'][2]['long_name']
+    return lng, lat, city
 
-def add_latlng_json(lat, lng, name, city, desc):
+def add_to_json(lng, lat, name, city, desc, address):
     with open('static/latlng.json') as file:
-            latlng = json.load(file)
-            features = latlng['features']
-            new_address = {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [float(lng), float(lat)]
-                },
-                "properties": {
-                    "title": name + " @ " + city,
-                    "description": desc,
-                    "identifier": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-                }
+        latlng = json.load(file)
+        features = latlng['features']
+        new_address = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [float(lng), float(lat)]
+            },
+            "properties": {
+                "title": name + " @ " + city,
+                "description": desc,
+                "identifier": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)),
+                "address": address,
+                "name": name
             }
-            features.append(new_address)
-            latlng['features'] = features
-            with open('static/latlng.json', 'w') as f:
-                json.dump(latlng, f, indent=4) # sort_keys=True
+        }
+        features.append(new_address)
+        latlng['features'] = features
+        with open('static/latlng.json', 'w') as f:
+            json.dump(latlng, f, indent=4) # sort_keys=True
 
 def json_to_list():
     with open('static/latlng.json') as file:
@@ -44,7 +48,59 @@ def json_to_list():
             new_list.append(feature['geometry']['coordinates'])
             new_list.append(feature['properties']['title'])
             new_list.append(feature['properties']['description'])
+            new_list.append(feature['properties']['identifier'])
             list_pass.append(new_list)
         return list_pass
-            
-add_latlng_json("38.907362", "-77.3967888", "Raunak Daga", "Oak Hill", "I need toiler paper")
+
+def remove_from_list(identifier_compare):
+    with open('static/latlng.json') as file:
+        latlng = json.load(file)
+        features = latlng['features']
+        for feature_index in range(len(features)):
+            if features[feature_index]['properties']['identifier'] == identifier_compare:
+                del features[feature_index]
+                break
+        latlng['features'] = features
+        with open('static/latlng.json', 'w') as f:
+            json.dump(latlng, f, indent=4) # sort_keys=True
+
+def add_to_list():
+    name = request.form['name']
+    address = request.form['address']
+    description = request.form['description']
+    # name = 'Rashad Philizaire'
+    # address = '6518 Trask Terrace, Alexandria, VA, 22315'
+    # description = 'I need toilet paper ASAP'
+
+    lng, lat, city = address_to_lnglat(address)
+    add_to_json(lng, lat, name, city, description, address)
+
+def text_info(identifier_compare, phone_number):
+    account_sid = ACCOUNT_SID
+    auth_token  = AUTH_TOKEN
+
+    client = Client(account_sid, auth_token)
+
+    name = address = desc = ''
+
+    with open('static/latlng.json') as file:
+        latlng = json.load(file)
+        features = latlng['features']
+        for feature_index in range(len(features)):
+            if features[feature_index]['properties']['identifier'] == identifier_compare:
+                name = features[feature_index]['properties']['name']
+                address = features[feature_index]['properties']['address']
+                desc = features[feature_index]['properties']['description']
+
+    body_of_text = f'''
+    Thank you Good Samaritan!\n\n{name} is waiting for you at {address}.\n\nHere's their request: {desc}\n\nThank you for everything you do, and remember to stay safe and take the proper precautions.
+    '''
+
+    to_number = phone_number
+    message = client.messages.create(
+        to=str(to_number), 
+        from_="+17147939326",
+        body=body_of_text
+    )
+
+    
